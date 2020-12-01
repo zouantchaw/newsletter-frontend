c610022f1cbc6dd00c27878f4d6916c9ef4369e8
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-12-01 15:12:59 -0500

    redux-thunk dependecy and store initialized

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +22,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
